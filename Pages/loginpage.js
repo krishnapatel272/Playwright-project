a4b3f4f8b06ebb618d36553a46311c3999776994
Krishna <krishna.patel2@hotmail.co.uk>
2026-08-22 16:10:53 +0100

    Removed hardcoded login details

--- Pages/loginpage.js
+++ Pages/loginpage.js
@@ -20,22 +20,22 @@ class LoginPage{
 
  async unsuccesfulLogin(username,password){
     await this.page.goto("https://www.saucedemo.com/");
-    await this.page.fill(this.username,'standard_user');
-    await this.page.fill(this.password,'test');
+    await this.page.fill(this.username,username);
+    await this.page.fill(this.password,password);
     await this.page.click(this.button);
   }
 
   async lockedAccount(username,password){
     await this.page.goto("https://www.saucedemo.com/");
-    await this.page.fill(this.username,'locked_out_user');
-    await this.page.fill(this.password,'secret_sauce');
+    await this.page.fill(this.username,username);
+    await this.page.fill(this.password,password);
     await this.page.click(this.button);
   }
 
   async slowLogin(username,password){
     await this.page.goto("https://www.saucedemo.com/");
-    await this.page.fill(this.username,'performance_glitch_user');
-    await this.page.fill(this.password,'secret_sauce');
+    await this.page.fill(this.username,username);
+    await this.page.fill(this.password,password);
     await this.page.click(this.button);
   }
 
